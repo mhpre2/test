c78883fbfb9647202e993c22babf9787969a7af3
serine <tskiril@gmail.com>
2019-04-12 15:18:28 +1000

    first commit, easy picard

---
+++ easy_view_picard.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: iso-8859-15 -*-
+
+import sys
+
+f = sys.argv[1]
+
+a = None
+b = None
+
+with open(f) as handler:
+    for  i in handler:
+        line = i.strip()
+        if line.startswith("#") or not line:
+            continue
+        if not a:
+            a = line
+        else:
+            b = line
+
+a = a.split("\t")
+b = b.split("\t")
+
+d = dict(zip(a,b))
+
+for k,v in d.items():
+    print('\t'.join((k,v)))
